45d80dd7d689390d6d5c8094686bf20d9dafd5e3
unknown <safweneessayess@gmail.com>
2024-04-09 13:21:33 +0100

    rectification

--- js/main.js
+++
@@ -1,6 +0,0 @@
-const navbarToggler = document.querySelector('.navbar-toggler');
-const navbarCollapse = document.querySelector('.navbar-collapse');
-
-navbarToggler.addEventListener('click', () => {
-  navbarCollapse.classList.toggle('show');
-});
